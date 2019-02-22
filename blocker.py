4dc95f765c92234c968c38e1f5774eb83ab4b447
Your name <andreychas1@gmail.com>
2019-02-22 02:21:31 +0200

    it works!

--- blocker.py
+++ blocker.py
@@ -1,4 +1,34 @@
 
-if __name__ == '__main__':
-    print('hi!')
+# works only for Windows for now
+HOSTS_PATH = r"C:\Windows\System32\drivers\etc\hosts"
 
+BLACKLIST = [
+        "www.facebook.com"
+        ]
+REDIRECT = "127.0.0.1"
+
+def block():
+    with open(HOSTS_PATH, "r+") as file:
+        content = file.read()
+        for website in BLACKLIST:
+            if website not in content:
+                # print(f"Blocking {website}...")
+                file.write(f"{REDIRECT} {website}\n")
+            
+def unblock():
+    with open(HOSTS_PATH, "r+") as file:
+        content = file.readlines()
+        file.seek(0)
+        for line in content:
+            if not any(website in line for website in BLACKLIST):
+                file.write(line)
+        file.truncate()
+
+if __name__ == '__main__':
+    user_action = input("Block or unblock (b/u)?\n")
+    if user_action == 'b':
+        block()
+    elif user_action == 'u':
+        unblock()
+    else:
+        print("Unknown command")
